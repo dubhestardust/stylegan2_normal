a21d3607807927bf83b2f0ed5437ed785d28d8a2
Dust <dubhestardust@gmail.com>
2021-05-03 19:47:11 +0800

    图片1tick，模型5tick

--- training/training_loop.py
+++ training/training_loop.py
@@ -151,7 +151,7 @@ def training_loop(
     spatial_augmentations   = False,    # Enable spatial augmentations from Zhao et al 2020b
     drange_net              = [-1,1],   # Dynamic range used when feeding image data to the networks.
     image_snapshot_ticks    = 1,       # How often to save image snapshots? None = only save 'reals.png' and 'fakes-init.png'.
-    network_snapshot_ticks  = 1,       # How often to save network snapshots? None = only save 'networks-final.pkl'.
+    network_snapshot_ticks  = 5,       # How often to save network snapshots? None = only save 'networks-final.pkl'.
     save_tf_graph           = False,    # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
     resume_pkl              = 'latest',     # Network pickle to resume training from, None = train from scratch.
